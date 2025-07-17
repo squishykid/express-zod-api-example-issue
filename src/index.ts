c6252832269453d8e7d8ed17e95fb669cc78dc98
zoton2 <zoton2@gmail.com>
2025-07-17 22:05:48 +0100

    add basic files

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import { z } from "zod"; // switching this to import from zod/v4 works fine
+import { Routing, createServer, createConfig, defaultEndpointsFactory } from "express-zod-api";
+
+const config = createConfig({
+  http: { listen: 8090 }, // port, UNIX socket or Net::ListenOptions
+  cors: false, // decide whether to enable CORS
+});
+
+const helloWorldEndpoint = defaultEndpointsFactory.build({
+  // method: "get" (default) or array ["get", "post", ...]
+  input: z.object({
+    name: z.string().optional(),
+  }),
+  output: z.object({
+    greetings: z.string(),
+  }),
+  handler: async ({ input: { name }, options, logger }) => {
+    logger.debug("Options:", options); // middlewares provide options
+    return { greetings: `Hello, ${name || "World"}. Happy coding!` };
+  },
+});
+
+const routing: Routing = {
+  v1: {
+    hello: helloWorldEndpoint,
+  },
+};
+
+createServer(config, routing);
